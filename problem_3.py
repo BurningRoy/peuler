4da3611d9c6029a50b38c0aa034d9342cd0c6cf8
BurningRoy <burningroy2012@gmail.com>
2014-09-26 02:48:16 -0700

    Update problem_3.py

--- problem_3.py
+++ problem_3.py
@@ -3,16 +3,20 @@
 # e.g
 # The prime factors of 13195 are 5, 7, 13 and 29
 
+import time
+start = time.time()
 divisor = 2
 dividend = 600851475143
+#dividend = 7
 
 while divisor < dividend:
     remainder = dividend % divisor
     if remainder == 0:
         dividend = dividend / divisor
-        divisor = 2
+        #divisor = 2
     else:
         divisor += 1
 else:
     print divisor
 
+print time.time() - start
